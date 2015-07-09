d8c73e2df4174c5703c0be9c2c313ff1ce931551
Edgar Hiram Hernandez Reyes <ehhernandez@uach.mx>
2015-07-09 13:26:53 -0600

    Se realiza limpieza de código general.

--- safedelete/src/play/modules/safedelete/com/uach/safedelete/SafeDeleteEnhancer.java
+++ safedelete/src/play/modules/safedelete/com/uach/safedelete/SafeDeleteEnhancer.java
@@ -1,23 +1,13 @@
 package com.uach.safedelete;
 
-//import com.google.code.morphia.annotations.Entity;
-//import com.google.code.morphia.annotations.Entity;
 import java.io.IOException;
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
-import java.util.logging.Level;
 import java.util.stream.Collectors;
 import javassist.CtClass;
-import javassist.CtField;
 import javassist.CtMethod;
-import javassist.Modifier;
 import javassist.NotFoundException;
-import play.Logger;
 import play.Play;
 import play.classloading.ApplicationClasses.ApplicationClass;
 import play.classloading.enhancers.Enhancer;
@@ -39,31 +29,14 @@ public class SafeDeleteEnhancer extends Enhancer {
 
         final String entityFullyQualifiedName = "com.google.code.morphia.annotations.Entity";
 
-//        final CtClass ctEntityClass = classPool.getCtClass(entityFullyQualifiedName);
-//        
-//        final Class annotationClass = classPool.getCtClass("com.google.code.morphia.annotations.Entity").toClass();
-//        ctEntityClass.defrost();
         //Sólo para modelos
-//        Class annotationClass = Play.classloader.getClassIgnoreCase(entityFullyQualifiedName);
-//        if (annotationClass == null) {
-//            annotationClass = classPool.getCtClass(entityFullyQualifiedName).toClass();
-//        }
         if (!ctClass.subtypeOf(ctModelClass)) {
             return;
         }
-//        if (!ctClass.hasAnnotation(annotationClass)) {
-//            return;
-//        }
         //Sólo si tiene la anotación de @Entity de morphia
         if (!checkCtClassForAnnotation(ctClass, entityFullyQualifiedName)) {
             return;
         }
-//        System.out.println("ctClass.getAnnotations() = " + ctClass.getAnnotations());
-//        System.out.println("ctClass.getAvailableAnnotations() = " + ctClass.getAvailableAnnotations());
-//        Arrays.asList(ctClass.getAnnotations()).forEach((a) -> a.getClass().getName());
-//        if (!Arrays.asList(ctClass.getAvailableAnnotations()).stream().anyMatch((ann) -> ann.getClass().getName().equals(entityFullyQualifiedName))) {
-//            return;
-//        }
 
         final String METHOD_NAME = "getReferencedBy";
 
@@ -71,8 +44,8 @@ public class SafeDeleteEnhancer extends Enhancer {
         try {
             CtMethod ctMethod = ctClass.getDeclaredMethod(METHOD_NAME);
             if (METHOD_NAME.equals(ctMethod.getName()) && ctMethod.getDeclaringClass().equals(ctClass)) {
-                return;
-//                throw new Exception(String.format("Error al realizar enhance de SafeDelete sobre la clase %s, el método %s ya se encuentra definido.", ctClass.getName(), METHOD_NAME));
+                //return;
+                throw new Exception(String.format("Error al realizar enhance de SafeDelete sobre la clase %s, el método %s ya se encuentra definido.", ctClass.getName(), METHOD_NAME));
             }
         } catch (NotFoundException noReferencedByMethod) {
         }
@@ -91,6 +64,7 @@ public class SafeDeleteEnhancer extends Enhancer {
                                 return tmpCtClass.subtypeOf(ctModelClass)
                                 //Debe tener tambíen la anotación
                                 && checkCtClassForAnnotation(tmpCtClass, entityFullyQualifiedName)
+                                //Debe tener algún campo que haga referencia a la clase actual
                                 && Arrays.asList(tmpCtClass.getDeclaredFields()).stream()
                                 .anyMatch(
                                         (ctField) -> {
@@ -196,25 +170,6 @@ public class SafeDeleteEnhancer extends Enhancer {
 
             i++;
         }
-//        for (int i = 0; i < ARRAY_SIZE; i++) {
-//                sb.append(String.format("%s[%d] = %s.class;", ARRAY_NAME, i));
-//                sb.append("\n");
-//        }
-//        sb.append(classes.stream()
-//                .map(
-//                        (clazz) -> {
-//                            String statement = String.format(
-//                                    "%s.add(%s.class);",
-//                                    SET_NAME,
-//                                    clazz.getName()
-//                            );
-//
-//                            return statement;
-//
-//                        }
-//                )
-//                .collect(Collectors.joining("\n")));
-//        sb.append("\n");
         sb.append(String.format("return %s;", ARRAY_NAME));
 
         return sb.toString();
